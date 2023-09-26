285f1181d9c3c98368ec6593fa6ae27cb173122a
DanielLucasL <72170522+DanielLucasL@users.noreply.github.com>
2023-09-26 19:37:35 -0300

    front-end-1-version

---
+++ BancoZ/src/java/java/Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java;
+
+/**
+ *
+ * @author danie
+ */
+public class Usuario {
+    private int id;
+    private String tipo;
+    private String nome;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+}
